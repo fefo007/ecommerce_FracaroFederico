046a86677ad4fad79c84b06ad58481f3f870f4de
Federico Fracaro <federicohfracaro@gmail.com>
2022-09-01 19:46:07 -0300

    finalAjustes6

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import './components/BuyForm.css'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { HashRouter } from 'react-router-dom';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
